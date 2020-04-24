c8433a41d7a7009f3b75872efbd5eee118e91710
Nathalia Pertile Lorenzi <nathalia.lorenzi@pampa.compasso>
2020-04-24 10:02:31 -0300

    crud generos

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Header from './Header'
 import Generos from './Generos'
 import NovoGenero from './NovoGenero'
 import EditarGenero from './EditarGenero'
-import axios from 'axios'
+
 import{
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 
 const Home = () => {
@@ -14,21 +15,16 @@ const Home = () => {
 }
 
 function App() {
-  const [data, setData] = useState({})
-  useEffect(() => {
-    axios.get('/api').then(res => {
-      setData(res.data)
-    })
-  }, [])
   return (
     <Router>
       <div>
         <Header />
-        <Route path='/'exact component={Home}/>
-        <Route path='/generos/:id' exact component={EditarGenero}/>
-        <Route path='/generos/novo' exact component={NovoGenero}/>
+        <Switch>
+        <Route path='/'exact component={Home}/>    
         <Route path='/generos' exact component={Generos}/>
-        <pre>{JSON.stringify(data)}</pre>
+        <Route path='/generos/novo' exact component={NovoGenero}/>
+        <Route path='/generos/:id' exact component={EditarGenero}/>
+        </Switch>
       </div>
     </Router>
     )
